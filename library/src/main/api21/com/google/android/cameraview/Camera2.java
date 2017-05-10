fee0b0ddb823c2136743186eaa7b82032a29d116
vuhung3990 <vuhung3990@gmail.com>
2017-05-10 14:49:02 +0700

    fix flash not work on androidSDK > 21

--- library/src/main/api21/com/google/android/cameraview/Camera2.java
+++ library/src/main/api21/com/google/android/cameraview/Camera2.java
@@ -553,7 +553,7 @@ class Camera2 extends CameraViewImpl {
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_ALWAYS_FLASH);
                 mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE,
-                        CaptureRequest.FLASH_MODE_OFF);
+                        CaptureRequest.FLASH_MODE_SINGLE);
                 break;
             case Constants.FLASH_TORCH:
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
@@ -565,13 +565,13 @@ class Camera2 extends CameraViewImpl {
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
                 mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE,
-                        CaptureRequest.FLASH_MODE_OFF);
+                        CaptureRequest.FLASH_MODE_SINGLE);
                 break;
             case Constants.FLASH_RED_EYE:
                 mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                         CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH_REDEYE);
                 mPreviewRequestBuilder.set(CaptureRequest.FLASH_MODE,
-                        CaptureRequest.FLASH_MODE_OFF);
+                        CaptureRequest.FLASH_MODE_SINGLE);
                 break;
         }
     }
